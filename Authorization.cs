dd02b2d4a1551250e1dceaac18a7e1aa3df28516
alpatov <kanfadez@gmail.com>
2022-02-24 15:16:31 +0300

    complete partial homework

---
+++ Authorization.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Authorization
+    {       
+        public static bool CheckAuthorization(string path)
+        {
+            string[] account = new string[2];
+            Account adminAccount = new Account("root", "GeekBrains");
+            try
+            {
+                account = File.ReadAllLines(path);
+                if (account[0] == adminAccount.Login) 
+                    if (account[1] == adminAccount.Password)
+                        return true;
+                    return false;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+
+        public struct Account
+        {
+            public Account(string login, string password)
+            {
+                Login = login;
+                Password = password;
+            }
+
+            public string Login { get; }
+            public string Password { get; }
+
+        }
+    }
+}
---
+++ MassiveClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class MassiveClass
+    {
+        private const int STEP = 1;
+        private const int LENGHT = 10;
+        private int[] massive = new int[LENGHT];
+
+        private int startData;
+        public MassiveClass(int startData)
+        {
+            this.startData = startData;
+            for (int i = 0; i < massive.Length; i++)
+            {
+                if (i == 0)
+                    massive[i] = startData;
+                else
+                    massive[i] = massive[i-1] + STEP;
+            }
+        }
+
+        private int Sum;
+
+        public int SumProperty
+        {
+            get 
+            {
+                for (int i = 0; i < massive.Length; i++)
+                {
+                    Sum = Sum + massive[i];
+                }
+                return Sum; 
+            }
+        }
+
+        public static int[] Inverse(MassiveClass enterData)
+        {
+            int[] inverseMassive = new int[enterData.massive.Length];
+            for (int i = 0; i < enterData.massive.Length; i++)
+            {
+                inverseMassive[i] = enterData.massive[i] * -1;
+            }
+            return inverseMassive;
+        }
+
+        public static int[] Multiply(MassiveClass enterData, int number)
+        {
+            int[] multiplyMassive = new int[enterData.massive.Length];
+            for (int i = 0; i < enterData.massive.Length; i++)
+            {
+                multiplyMassive[i] = enterData.massive[i] * number;
+            }
+            return multiplyMassive;
+        }
+
+
+
+    }
+}
